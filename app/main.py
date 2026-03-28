1efccda56bfdacb9209d2d1cada327e0ad3276c1
pawunder1992 <pavelkryvutsa@gmail.com>
2026-03-28 16:16:58 +0200

    Solved problems with positions of arguments in cinema_visit

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     persons = []
     for customer in customers:
         persons.append(Customer(customer["name"], customer["food"]))
